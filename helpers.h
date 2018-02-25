43f6065742e880e079f0e1b8603cd213995e8b49
Akshay Khole <akshay@reversehack.in>
2018-02-25 10:25:12 -0800

    Updates helper for full filter

--- helpers.h
+++ helpers.h
@@ -16,10 +16,13 @@
 
 using namespace std;
 
+static float STATIC_ONE_OVER_SQRT_2PI = 1/sqrt(2*M_PI) ;
 class Helpers {
  public:
 
   //definition of one over square root of 2*pi:
+  float ONE_OVER_SQRT_2PI = 1/sqrt(2*M_PI) ;
+
   /*****************************************************************************
    * normpdf(X,mu,sigma) computes the probability function at values x using the
    * normal distribution with mean mu and standard deviation std. x, mue and
@@ -27,7 +30,31 @@ class Helpers {
    * The normal pdf is y=f(x;mu,std)= 1/(std*sqrt(2pi)) e[ -(x−mu)^2 / 2*std^2 ]
    *****************************************************************************/
   static float normpdf(float x, float mu, float std) {
-    return ((1/sqrt(2*M_PI) )/std)*exp(-0.5*pow((x-mu)/std,2));
+    return (STATIC_ONE_OVER_SQRT_2PI/std)*exp(-0.5*pow((x-mu)/std,2));
+  }
+
+  //static function to normalize a vector:
+  static std::vector<float> normalize_vector(std::vector<float> inputVector){
+
+    //declare sum:
+    float sum = 0.0f;
+
+    //declare and resize output vector:
+    std::vector<float> outputVector ;
+    outputVector.resize(inputVector.size());
+
+    //estimate the sum:
+    for (unsigned int i = 0; i < inputVector.size(); ++i) {
+      sum += inputVector[i];
+    }
+
+    //normalize with sum:
+    for (unsigned int i = 0; i < inputVector.size(); ++i) {
+      outputVector[i] = inputVector[i]/sum;
+    }
+
+    //return normalized vector:
+    return outputVector;
   }
 
 };
