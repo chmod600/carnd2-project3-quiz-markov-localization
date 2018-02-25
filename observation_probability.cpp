31c22b74cd61cad6fc288d1bb80fe908b1cfc0bc
Akshay Khole <akshay@reversehack.in>
2018-02-24 17:02:41 -0800

    Observation model programmed

--- observation_probability.cpp
+++ observation_probability.cpp
@@ -1,62 +1,105 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include "helpers.h"
 
+#include "helpers.h"
 using namespace std;
 
-//set standard deviation of control:
-float control_stdev = 1.0f;
-
-//meters vehicle moves per time step
-float movement_per_timestep = 1.0f;
-
-//number of x positions on map
-int map_size = 25;
-
-//define landmarks
-std::vector<float> landmark_positions { 5, 10, 12, 20 };
+//function to get pseudo ranges
 std::vector<float> pseudo_range_estimator(std::vector<float> landmark_positions,
                                           float pseudo_position);
+
+//observation model: calculates likelihood prob term based on landmark proximity
+float observation_model(std::vector<float> landmark_positions, std::vector<float> observations,
+                        std::vector<float> pseudo_ranges, float distance_max,
+                        float observation_stdev);
+
 int main() {
-  // step through each pseudo position x (i)
+
+  //set observation standard deviation:
+  float observation_stdev = 1.0f;
+
+  //number of x positions on map
+  int map_size = 25;
+
+  //set distance max
+  float distance_max = map_size;
+
+  //define landmarks
+  std::vector<float> landmark_positions {5, 10, 12, 20};
+
+  //define observations
+  std::vector<float> observations {5.5, 13, 15};
+
+  //step through each pseudo position x (i)
   for (unsigned int i = 0; i < map_size; ++i) {
     float pseudo_position = float(i);
-    // get pseudo ranges
-    std::vector<float> pseudo_ranges = pseudo_range_estimator(landmark_positions, pseudo_position);
+
+    //get pseudo ranges
+    std::vector<float> pseudo_ranges = pseudo_range_estimator(landmark_positions,
+                                                              pseudo_position);
+
+    //get observation probability
+    float observation_prob = observation_model(landmark_positions, observations,
+                                               pseudo_ranges, distance_max,
+                                               observation_stdev);
 
     //print to stdout
-    if (pseudo_ranges.size() > 0) {
-      for (unsigned int s = 0; s < pseudo_ranges.size(); ++s) {
-        std::cout << "x: " << i << "\t" << pseudo_ranges[s] << endl;
-      }
-      std::cout << "-----------------------" << endl;
-    }
+    std::cout << observation_prob << endl;
   }
 
   return 0;
 };
 
-//TODO: Complete pseudo range estimator function
-std::vector<float> pseudo_range_estimator(std::vector<float> landmark_positions, float pseudo_position) {
+std::vector<float> pseudo_range_estimator(std::vector<float> landmark_positions,
+                                          float pseudo_position) {
 
   //define pseudo observation vector:
   std::vector<float> pseudo_ranges;
 
   //loop over number of landmarks and estimate pseudo ranges:
-  //YOUR CODE HERE
+  for (unsigned int l=0; l< landmark_positions.size(); ++l) {
 
-  for(unsigned int i = 0; i < landmark_positions.size(); ++i) {
-    int diff = landmark_positions[i] - pseudo_position;
-    if (diff > 0) {
-      pseudo_ranges.push_back(diff);
+    //estimate pseudo range for each single landmark
+    //and the current state position pose_i:
+    float range_l = landmark_positions[l] - pseudo_position;
+
+    //check if distances are positive:
+    if (range_l > 0.0f) {
+      pseudo_ranges.push_back(range_l);
     }
   }
 
   //sort pseudo range vector:
-  //YOUR CODE HERE
-
   sort(pseudo_ranges.begin(), pseudo_ranges.end());
 
   return pseudo_ranges;
+}
+
+//TODO Complete the observation model function
+//calculates likelihood prob term based on landmark proximity
+float observation_model(std::vector<float> landmark_positions,
+                        std::vector<float> observations,
+                        std::vector<float> pseudo_ranges,
+                        float distance_max,
+                        float observation_stdev) {
+
+  //YOUR CODE HERE
+  float distance_probability = 1.0f;
+  float min = 0.0;
+
+  for(unsigned int i = 0; i < observations.size(); ++i) {
+    if(pseudo_ranges.size() > 0) {
+      min = pseudo_ranges[0];
+      pseudo_ranges.erase(pseudo_ranges.begin());
+    } else {
+      min = distance_max;
+    }
+
+    distance_probability *= Helpers::normpdf(observations[i],
+                                             min,
+                                             observation_stdev);
+  }
+
+  return distance_probability;
 }
